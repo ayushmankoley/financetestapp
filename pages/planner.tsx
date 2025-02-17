2354ced823f2ce332738465ed8fe0e9903e96554
ayushmankoley <ayushmankoley1@gmail.com>
2025-02-17 15:26:04 +0530

    fixed bugs

--- pages/planner.tsx
+++ pages/planner.tsx
@@ -241,7 +241,7 @@ const PortfolioPlanner = () => {
         pdf.setFont('helvetica', 'bold');
         pdf.setFontSize(12);
         pdf.text('Detailed Allocation', margin, yAfterChart);
-        var yPosition = yAfterChart + 10;
+        const yPosition = yAfterChart + 10;
         
         pdf.setFont('helvetica', 'normal');
         pdf.setFontSize(10);
