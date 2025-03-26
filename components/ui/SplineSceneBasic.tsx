c754749de6f6defb823ec4c137e4ac106c584960
Souherdya Sarkar <souherdyasarkar07@gmail.com>
2025-03-26 20:32:41 +0530

    Small changes

--- components/ui/SplineSceneBasic.tsx
+++ components/ui/SplineSceneBasic.tsx
@@ -4,8 +4,8 @@ import { SplineScene } from "@/components/ui/spline";
 export function SplineSceneBasic() {
   return (
     <SplineScene 
-      scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
-      className="w-full h-full"
+      scene="https://prod.spline.design/e5qRCjL1QC9CE1Ni/scene.splinecode"
+      className="w-[90%] h-[80%]"
     />
   )
 }
